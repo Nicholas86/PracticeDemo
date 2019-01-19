07114fd288ca761c25d1fc83baaa4e77b35dbaee
泽娄 <970649831@qq.com>
2019-01-19 22:35:42 +0800

    runloop

---
+++ TestDelegate/Demo/Runloop/ViewModel/NRunLopp.h
@@ -0,0 +1,17 @@
+//
+//  NRunLopp.h
+//  TestDelegate
+//
+//  Created by 泽娄 on 2019/1/19.
+//  Copyright © 2019 a. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NRunLopp : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TestDelegate/Demo/Runloop/ViewModel/NRunLopp.m
@@ -0,0 +1,13 @@
+//
+//  NRunLopp.m
+//  TestDelegate
+//
+//  Created by 泽娄 on 2019/1/19.
+//  Copyright © 2019 a. All rights reserved.
+//
+
+#import "NRunLopp.h"
+
+@implementation NRunLopp
+
+@end
